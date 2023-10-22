0c54c109bbe7c91645a472338f8939a69f4b8a76
ERMIMK <87019762+ERMIMK@users.noreply.github.com>
2023-10-22 18:36:59 -0400

    Initial Commit

---
+++ Spartagram/SpartagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpartagramApp.swift
+//  Spartagram
+//
+//  Created by Ermiyas Mesfin on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SpartagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
